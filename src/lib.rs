a14d9bcce658bdd849b21ee939f23e0bf46a5358
Kenan Rhoton <kenan.rhoton.com>
2018-10-02 11:08:59 +0200

    Relative Home Path

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,38 @@
 use std::io::Write;
+extern crate dirs;
 
-const DATA_FILE: &str = "data";
+const DATA_FILE: &str = ".stakdata";
+
+fn data_file() -> String {
+    match dirs::home_dir() {
+        Some(mut path) => {
+            path.push(DATA_FILE);
+            path.to_string_lossy().to_string()
+        },
+        None => DATA_FILE.to_string(),
+    }
+}
 
 pub fn load_data() -> Vec<String> {
-        std::fs::read_to_string(DATA_FILE)
-            .expect("File could not be read from!")
-            .split("\n")
-            .filter(|x| !x.is_empty())
-            .map(|x| x.to_string())
-            .collect()
+    let data = &data_file();
+    std::fs::read_to_string(data)
+        .expect(&format!("File could not be read from \"{}\"!", data))
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|x| x.to_string())
+        .collect()
 }
 
 pub fn append_data(new_line: String) {
-    let mut file = if std::path::Path::new(DATA_FILE).exists() {
+    let data = &data_file();
+    let mut file = if std::path::Path::new(data).exists() {
         println!("APPENDING!");
-        std::fs::OpenOptions::new().append(true).open(DATA_FILE)
+        std::fs::OpenOptions::new().append(true).open(data)
     } else {
         println!("CREATING!");
-        std::fs::File::create(DATA_FILE)
+        std::fs::File::create(data)
     }.expect("File could not be written to!");
 
-    file.write_fmt(format_args!("\n{}", new_line.to_string()))
+    file.write_fmt(format_args!("{}\n", new_line.to_string()))
         .expect("Something went wrong!");
 }
